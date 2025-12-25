423cafde4d9a99ca345a952ad4afb90fe4f08bea
jesagui28mx <jesagui28@gmail.com>
2025-12-25 13:31:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,10 @@ class PDFReport(FPDF):
         self.set_text_color(100, 100, 100) # Color gris para no distraer
         
         disclaimer = (
-            "AVISO LEGAL: Los rendimientos y montos en este documento son estimados, es una simulación de carácter exclusivamente" 
+            "AVISO LEGAL: Los rendimientos y montos en este documento son estimativos, una simulación de carácter exclusivamente" 
             "informativo y no constituye una cotización formal"
             "ni una oferta vinculante por parte de una compañía de seguros."
-            "Para obtener una cotización oficial y proceder a la contratación, por favor comuníquese con su asesor certificado."
+            "Para obtener una cotización oficial y proceder a la contratación, por favor comuníquese con su asesor."
         )
         self.multi_cell(0, 3, disclaimer, 0, 'C') # Centrado y con salto de línea automático
 
